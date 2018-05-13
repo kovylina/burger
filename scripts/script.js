9db1b68e3e93bb54ebebc90d50633b36d6c8d955
Tatyana Kovylina <tkovylina@gmail.com>
2018-05-13 23:17:40 +0300

    open and close mobile-menu

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+const openTrigger = document.querySelector('.hamburger-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const closeTrigger = document.querySelector('.mobile-menu__btn')
+
+console.log(openTrigger);
+
+openTrigger.addEventListener ('click', event => {
+  event.preventDefault();
+
+  if(!mobileMenu.classList.contains('mobile-menu--active'))
+    mobileMenu.classList.add('mobile-menu--active');
+});
+
+closeTrigger.addEventListener ('click', function(e) {
+  e.preventDefault();
+
+  if(mobileMenu.classList.contains('mobile-menu--active'))
+    mobileMenu.classList.remove('mobile-menu--active');
+});
+
